86c069b3462434f4ee93256b4141b5c7bae85af6
Manuel Solis <manolocsolis@gmail.com>
2020-03-03 16:54:55 -0500

    Completed home page

--- src/App.js
+++ src/App.js
@@ -4,22 +4,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+      <section class="navbar">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li class="active"><a href="#">About me</a></li>
+          <li><a href="#">Projects</a></li>
+        </ul>
+      </section>
+      
+    
+  <section class="section-header">
+    <section class="bubble-box-container">
+      <section>
+      <img class="headshot" src="/images/Manuel-Headshot.jpeg" alt="Manuel Solis" width="200" height="200"/>
+      <section class="bubble-box">
+        <p>Manuel Solis</p>
+        <p class="bubble-box-span">Full-Stack Dev | Marketing Guru | Mentor</p>
+      </section>
+      </section>
+    </section>
+    </section>
+    <section class="quote">
+      <h2> "If you think you can or you can't, either way you are right" - H. Ford</h2>
+    </section>
+    <section class="section-projects">
+      <section class="section-projects-images">
+      <a class="button" href="/brand.html">
+            <img class="square" src="/images/branding.jpg" alt="Branding" height="140" width="auto"/>
+          </a>
+          <a href="/leadership.html">
+            <img class="square" src="/images/leadership.jpg" alt="leadership" height="150" width= "auto"/>
+          </a>
+          <a href="/projects.html">
+            <img class="square" src="/images/code.png" alt="Bomb Sniffer" height="135" width= "180"/>
+          </a>
+      </section>
+    </section>
+    <section class="section-header-react"></section>
+  </div>
   );
 }
 
